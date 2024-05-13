e7ebf768a26791711e1a32003f4a4572c32f2469
yamaji <yamaji@netspice.co.jp>
2024-05-14 00:42:30 +0900

    開発環境構築関連の不具合を修正しました。

--- containers/image-analysis-app/entrypoint.sh
+++ containers/image-analysis-app/entrypoint.sh
@@ -4,5 +4,6 @@ cd /workspace
 # 依存ライブラリをインストールする
 pip install -r requirements.lock
 
+python manage.py migrate
+
 python manage.py runserver 0.0.0.0:8000
-# sleep infinity
--- containers/mysql/init.sql
+++ containers/mysql/init.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE image_analysis;
+
 CREATE USER 'webapp'@'%' IDENTIFIED BY 'mysql';
+
 GRANT ALL PRIVILEGES ON *.* TO 'webapp'@'%';
+
 FLUSH PRIVILEGES;
